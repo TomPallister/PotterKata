b7350697c7d4e4f928c2e62e1655b84246a2775a
Tom Pallister <Tom.Pallister@moonpig.com>
2016-07-12 09:05:48 +0100

    potter kata

---
+++ PotterKata/Calculator.cs
@@ -0,0 +1,79 @@
+﻿namespace PotterKata
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class DiscountCalculator
+    {
+        private readonly decimal[] _discountRates = {1, 0.95m, 0.90m, 0.80m, 0.75m};
+
+        private readonly int[] _discounts = new int[5];
+
+        public decimal CalculatePrice(List<int> order)
+        {
+            if (order == null || !order.Any())
+            {
+                return 0.0m;
+            }
+
+            var booksInOrder = SortBooks(order);
+
+            CalculateDiscounts(booksInOrder);
+
+            OptimiseDiscounts();
+
+            return _discounts.Select((t, i) => 8*(i + 1)*t*_discountRates[i]).Sum();
+        }
+
+        protected void OptimiseDiscounts()
+        {
+            while (_discounts[2] > 0 && _discounts[4] > 0)
+            {
+                _discounts[2]--;
+                _discounts[4]--;
+                _discounts[3] += 2;
+            }
+        }
+
+        protected void CalculateDiscounts(int[] booksInOrder)
+        {
+            while (true)
+            {
+                if (booksInOrder == null) return;
+
+                var differentFromZero = 0;
+
+                for (var i = 0; i < booksInOrder.Length; i++)
+                {
+                    if (booksInOrder[i] <= 0)
+                    {
+                        continue;
+                    }
+
+                    differentFromZero++;
+
+                    booksInOrder[i]--;
+                }
+
+                if (differentFromZero <= 0)
+                {
+                    return;
+                }
+
+                _discounts[differentFromZero - 1] += 1;
+            }
+        }
+
+        private int[] SortBooks(IEnumerable<int> order)
+        {
+            var result = new int[5];
+
+            foreach (var book in order)
+            {
+                result[book - 1]++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ PotterKata/PotterDiscountCalculatorTests.cs
@@ -0,0 +1,43 @@
+﻿namespace PotterKata
+{
+    using System.Collections.Generic;
+    using Shouldly;
+    using Xunit;
+
+    public class PotterDiscountCalculatorTests
+    {
+        private decimal Price(List<int> books)
+        {
+            var calculator = new DiscountCalculator();
+            return calculator.CalculatePrice(books);
+        }
+
+        private void AssertEqual(decimal expectedprice, decimal price)
+        {
+            expectedprice.ShouldBe(price);
+        }
+
+        [Fact]
+        public void Test()
+        {
+            AssertEqual(0, Price(new List<int>()));
+            AssertEqual(8, Price(new List<int> {1}));
+            AssertEqual(8, Price(new List<int> {2}));
+            AssertEqual(8, Price(new List<int> {3}));
+            AssertEqual(8, Price(new List<int> {4}));
+            AssertEqual(8, Price(new List<int> {5}));
+            AssertEqual(8*2, Price(new List<int> {1, 1}));
+            AssertEqual(8*3, Price(new List<int> {1, 1, 1}));
+            AssertEqual(8*2*0.95m, Price(new List<int> {1, 2}));
+            AssertEqual(8*3*0.9m, Price(new List<int> {1, 3, 5}));
+            AssertEqual(8*4*0.8m, Price(new List<int> {1, 2, 3, 5}));
+            AssertEqual(8*5*0.75m, Price(new List<int> {1, 2, 3, 4, 5}));
+            AssertEqual(8 + 8*2*0.95m, Price(new List<int> {1, 1, 2}));
+            AssertEqual(2*(8*2*0.95m), Price(new List<int> {1, 1, 2, 2}));
+            AssertEqual(8*4*0.8m + 8*2*0.95m, Price(new List<int> {1, 1, 2, 3, 4, 4}));
+            AssertEqual(8 + 8*5*0.75m, Price(new List<int> {1, 2, 3, 3, 4, 5}));
+            AssertEqual(2*(8*4*0.8m), Price(new List<int> {1, 1, 2, 2, 3, 4, 4, 5}));
+            AssertEqual(3*(8*5*0.75m) + 2*(8*4*0.8m),Price(new List<int>{ 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5,5 }));
+        }
+    }
+}
